ff12c3938da09fd05c16f9f193c2f7cd18f4ba16
Adrian <adrianvilcu96@gmail.com>
2020-05-24 20:39:01 +0200

    settings updated

--- MemeLife/settings.py
+++ MemeLife/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -25,7 +24,7 @@ SECRET_KEY = '&_kmo%o$r)yoobo+kd+(4si7remkd%jj!=ctom4rc$g1jps-kn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'memelifedaw.herokuapp.com']
 
 
 # Application definition
@@ -135,5 +134,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-django_heroku.settings(locals())
